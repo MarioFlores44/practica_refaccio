70679fbc53105388f8fc9283043250868c4a308e
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2023-05-13 21:04:48 +0200

    Add files via upload

---
+++ src/Order.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Order {
+    private List<OrderLineItem> lineItems;
+    private double taxRate;
+
+    public Order(List<OrderLineItem> lineItems, double taxRate) {
+        this.lineItems = lineItems;
+        this.taxRate = taxRate;
+    }
+
+    public double calculateTotalPrice() {
+        double subtotal = 0.0;
+        for (OrderLineItem item : lineItems) {
+            subtotal += item.getPrice();
+        }
+        // REFAC: Eliminar varaible innecesaria
+        //double tax = subtotal * taxRate;
+        return subtotal + (subtotal * taxRate);
+    }
+}
